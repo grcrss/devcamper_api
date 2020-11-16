cd9b939d19f279cbbb2d55c7d598f98ca278ac1e
GRACE Kelana <grace.kelana@ibm.com>
2020-11-16 16:00:22 +0700

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+//connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
+//Dev logging middleware
+// app.use(logger);
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
-
+//Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+})
